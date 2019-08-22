95492a5537eb399c9fc434ed284cc6782eea4b2a
Diogo Borges <diogosousa2266@gmail.com>
2019-08-22 01:06:05 -0400

    Implementando seed down de lançamentos para não ocorrer ao executar api no docker

---
+++ api/db/seeders/4-inserir-lancamento.js
@@ -0,0 +1,9 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.resolve();
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Lancamento", null, {});
+  }
+};
